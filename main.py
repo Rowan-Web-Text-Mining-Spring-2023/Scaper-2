372ba89bbf742b629db418ff6e8a70cac900280a
mitche54 <rmitchell122w@gmail.com>
2023-01-27 21:22:57 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import snscrape.modules.twitter as sntwitter
+import csv
+from datetime import date
+
+twitter_handles = ['Cristiano', 'alexmorgan13', 'CHARLESBARKLEY', 'terrellowens', 'thecooleyzone', 'RyanWhitney6', 'CaroWozniacki', 'ShawnJohnson', 'paulpierce34', 'AaronRodgers12', 'KimKardashian']
+start = '2010-12-25' #YYYY-MM-DD
+end = date.today() #YYYY-MM-DD
+for twitter_handle in twitter_handles:
+    tweets = []
+    print(twitter_handle)
+    query = "(from:{}) until:{} since:{}".format(twitter_handle, end, start)
+    limit = 200
+
+    for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+        if len(tweets) == limit:
+            break
+        else:
+            tweets.append([tweet.url, tweet.date, tweet.content, tweet.id, tweet.user,  tweet.replyCount, tweet.retweetCount, tweet.likeCount,   tweet.quoteCount, tweet.conversationId, tweet.lang, tweet.source, tweet.coordinates])
+        
+    file = open('output.csv', 'a+', encoding='utf-8',newline='')
+    with file:
+        write = csv.writer(file)
+        write.writerows(tweets)
+
+print('Finished')
