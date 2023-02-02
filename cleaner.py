b4f17e7e01f4c9cb0149947e74b82b6d37604081
mitche54 <rmitchell122w@gmail.com>
2023-02-02 12:24:26 -0500

    better emoji removal and inclusive of all available characters in twitter usernames

--- cleaner.py
+++ cleaner.py
@@ -1,30 +1,10 @@
 import contractions
 import csv
 import re
+import demoji
 
-#Removes any emoji, takes arg(str)
-def remove_emojis(data):
-    emoj = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        u"\U00002500-\U00002BEF"  # chinese char
-        u"\U00002702-\U000027B0"
-        u"\U00002702-\U000027B0"
-        u"\U000024C2-\U0001F251"
-        u"\U0001f926-\U0001f937"
-        u"\U00010000-\U0010ffff"
-        u"\u2640-\u2642" 
-        u"\u2600-\u2B55"
-        u"\u200d"
-        u"\u23cf"
-        u"\u23e9"
-        u"\u231a"
-        u"\ufe0f"  # dingbats
-        u"\u3030"
-                      "]+", re.UNICODE)
-    return re.sub(emoj, '', data)
+def remove_new_lines(value):
+    return ''.join(value.splitlines())
 
 many = []    #List to store all tweets after cleaning
 #Loop through all tweets in csv file
@@ -37,11 +17,12 @@ with open('tweets.csv', encoding='utf-8') as csv_file:
 
         #Checking each element of line
         for item in row:
+            item = remove_new_lines(item)    #removes new lines in text
             item = item.lower()    #lowercases text
             item = contractions.fix(item)    #removes contractions
             item = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', 'xxhyperlink', item)     #removes hyperlinks
-            item = remove_emojis(item)      #removes emojis
-            item = re.sub(r'@[A-Za-z0-9]+', 'xxhandle', item)     #removes @handles
+            item = demoji.replace(item, "")      #removes emojis
+            item = re.sub(r'@[A-Za-z0-9_]+', 'xxhandle', item)     #removes @handles
             new_row.append(item)
         print(new_row)
         many.append(new_row)
